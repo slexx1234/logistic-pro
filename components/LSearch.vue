989ba14add66dbe8dd54b297205891bb10657ff6
Alex <slexx1234@gmail.com>
2018-04-14 11:59:38 +0300

    Сделал вывод результатов поиска

--- components/LSearch.vue
+++ components/LSearch.vue
@@ -2,6 +2,30 @@
     <div class="search">
         <input class="search__input" placeholder="Поиск посылок"/>
         <icon class="search__icon" name="search"/>
+        <div class="search__result items__content" v-show="false">
+            <div class="search__item">
+                <div class="search__item-top">
+                    <div class="items__code">LP09080-RS</div>
+                    <div class="items__status items__status--send"><icon name="paper-plane"/> В пути</div>
+                    <div class="items__sender">Бойдашёв В.Н.</div>
+                </div>
+                <div class="search__item-bottom">
+                    <div class="items__direction">Киров SV1 <icon name="angle-right"/> Москва SV47</div>
+                    <div class="items__time">5 дней назад</div>
+                </div>
+            </div>
+            <div class="search__item">
+                <div class="search__item-top">
+                    <div class="items__code">LP09080-RS</div>
+                    <div class="items__status items__status--handed"><icon name="check-circle"/> Вручено</div>
+                    <div class="items__sender">Бойдашёв В.Н.</div>
+                </div>
+                <div class="search__item-bottom">
+                    <div class="items__direction">Киров SV1 <icon name="angle-right"/> Москва SV47</div>
+                    <div class="items__time">58 дней назад</div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,6 @@
 <template>
     <div>
         <nuxt/>
-        <l-pixel-perfect/>
     </div>
 </template>
 
-<script>
-    import LPixelPerfect from "../components/LPixelPerfect";
-
-    export default {
-        components: {LPixelPerfect}
-
-    }
-</script>

25555edb779dcfeb691c0024df3f7d4107511a78
Alex <slexx1234@gmail.com>
2018-04-14 11:12:13 +0300

    Сделал вывод контента

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,34 @@
 <template>
     <section>
         <l-header/>
-        Content
+        <l-container>
+            <table class="items">
+                <thead class="items__header">
+                    <tr>
+                        <th>Трек код</th>
+                        <th>Направление</th>
+                        <th>Местоположение</th>
+                        <th>Статус</th>
+                        <th>Выжность</th>
+                        <th>Отправитель</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody class="items__content">
+                    <tr>
+                        <td class="items__code">LP09080-VR</td>
+                        <td class="items__direction">Киров SV1 <icon name="angle-right"/> Москва SV47</td>
+                        <td class="items__location"><icon name="location-arrow"/> Тула SK1</td>
+                        <td class="items__status items__status--handed"><icon name="check-circle"/> Вручено</td>
+                        <td class="items__important items__important--high"><icon name="exclamation-triangle"/> Срочная доставка</td>
+                        <td class="items__sender">Бойдашов В.Н.</td>
+                        <td class="items__checkbox">
+                            <l-checkbox/>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </l-container>
         <l-footer/>
     </section>
 </template>
@@ -9,9 +36,13 @@
 <script>
     import LHeader from "../components/LHeader";
     import LFooter from "../components/LFooter";
+    import LContainer from "../components/LContainer";
+    import LCheckbox from "../components/LCheckbox";
 
     export default {
         components: {
+            LCheckbox,
+            LContainer,
             LFooter,
             LHeader
 
